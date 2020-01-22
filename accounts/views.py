cf60c6aa943c685586a2993888653fc2c9184886
LeoChaeeeun <cheun0327@gmail.com>
2020-01-22 21:16:13 +0900

    8 | 회원가입 시에 next인자 처리하기
    회원가입 후, 이전의 페이지로 돌아가기

--- accounts/views.py
+++ accounts/views.py
@@ -8,27 +8,29 @@ from django.views.generic import CreateView
 from .forms import SignupForm
 
 
-'''def signup(request):
+def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             user=form.save()    
             # 이부분에서 회원가입이 완료됨 / 여기에서 로그인 처리 해주면됨
             auth_login(request, user)
-            return redirect('profile')
+            next_url=request.GET.get('next') or 'profile'
+            return redirect(next_url)
     else:
         form=SignupForm()
     return render(request, 'accounts/signup.html', {
         'form': form,
-    })'''
-
+    })
+'''
 class SignupView(CreateView):
     model=User
     form_class=SignupForm
     template_name = 'accounts/signup.html'
 
     def get_success_url(self):
-        return resolve_url('profile')
+        next_url=self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
 
     def form_valid(self, form):
         user = form.save()
@@ -36,6 +38,8 @@ class SignupView(CreateView):
         return redirect(self.get_success_url())
 
 signup=SignupView.as_view()
+'''
+
 @login_required     # 로그인 상황을 보장하기 위한 코드
 def profile(request):
     return render(request, 'accounts/profile.html')
