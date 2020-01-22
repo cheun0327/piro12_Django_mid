1ce3065de59f97f50a86f1372832c2c3f77aabd4
LeoChaeeeun <cheun0327@gmail.com>
2020-01-22 16:16:46 +0900

    3 | profile page
    각 user마다 profile보여주는 페이지 제작
    @login_required

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+@login_required     # 로그인 상황을 보장하기 위한 코드
+def profile(request):
+    return render(request, 'accounts/profile.html')
--- askdjango/urls.py
+++ askdjango/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('', lambda req: redirect('blog:post_list')),   # 제일 첫 화면이 블로그
+    path('', lambda req: redirect('blog:post_list'), name='root'),   # 제일 첫 화면이 블로그
     # url reverse사용하면 좋다
     path('accounts/', include('accounts.urls')),
 ]
