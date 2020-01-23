1c2d7449aa7f7212d4e2c5d9683e7346ec8aaef0
LeoChaeeeun <cheun0327@gmail.com>
2020-01-23 10:39:47 +0900

    9 | 로그인 아이디로 이메일 사용
    이메일 형식 검사
    이메일 필드에 username으로서의 이메일 저장

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,25 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import validate_email
+
 
 class SignupForm(UserCreationForm):
-    pass
+
+    def clean_username(self):       # 이메일 형식 검사
+        value = self.cleaned_data.get('username')
+        if value:       # 값이 있을 경우
+            validate_email(value)
+        return value
+
+    def save(self, commit=True):        # 이메일 필드에 이메일 저장 추가
+        user=super().save(commit=False)
+        user.email=user.username
+        if commit:
+            user.save()
+        return user
+
+    '''
+    def __init__(self, *args, **kwargs):
+        super().__init__(self,*args,**kwargs)
+        self.fields['username'].validators = [validate_email]
+        self.fields['username'].help_text = 'Enter Email Format'
+        '''
